3576ec59f12036e2f083fcbfbc5ffcf3a072465d
Gen9ft <52384791+Gen9ft@users.noreply.github.com>
2021-08-19 23:47:43 +0300

    добавление поведения кнопок
    Добавил интерактив на сайт с помощью языка JavaScript и библиотеки jQuery. Написал скрипт подсветки этажей и изменение счетчика этажей.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  var currentFloor = '2'; //переменная, где храниться текущий этаж
+  var floorPath= $('.home-img path'); //каждый отдельный этаж в svg
+  var counterUp = $('.counter-up'); /*кнопка увеличение этажа */
+  var counterDown = $('.counter-down'); /*кнопка уменьшения этажа */
+ floorPath.on('mouseover', function(){
+   floorPath.removeClass('current-floor');
+   currentFloor=$(this).attr('data-floor');
+  $('.counter').text(currentFloor);
+  });
+
+/*поведение вверхней кнопки */
+
+counterUp.on('click', function() { //отслежиаем клик по кнопке вверх
+if (currentFloor < 18){ // определяем переменную с этажом, оно не должно быть больше 18
+  currentFloor++;
+usCurrenFloor = currentFloor.toLocaleString('en-Us', {minimumIntegerDigits:2 , useGrouping: false});/*переделывание что бы вместо 2 было 02 */
+ $('.counter').text(usCurrenFloor);
+floorPath.removeClass('current-floor'); /*очищение от класса *от синего на доме */ // удоление класса
+  $(`[data-floor=${usCurrenFloor}]`).toggleClass('current-floor'); /*добавление синей дичи*/ // подсвечиваем текущий этаж
+}
+});
+
+////////////////////////
+
+counterDown.on('click', function() { /*поведение нижней кнопки */
+if (currentFloor > 2){
+  currentFloor--;
+usCurrenFloor = currentFloor.toLocaleString('en-Us', {minimumIntegerDigits:2 , useGrouping: false}); /*переделывание что бы вместо 2 было 02 */
+ $('.counter').text(usCurrenFloor);
+
+floorPath.removeClass('current-floor'); /*очищение от класса *от синего на доме */
+  $(`[data-floor=${usCurrenFloor}]`).toggleClass('current-floor'); /*добавление синей дичи*/
+}
+});
+
+
+});
